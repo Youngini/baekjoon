64c40629d5647fe7ceeb8c4b5f3ce90c5194eb29
Youngini <hardworkingini@gmail.com>
2022-09-09 23:04:19 +0900

    [재귀] 재귀의 귀재_앞 코드에서 불필요한 변수 제거

--- baek25501_py.py
+++ baek25501_py.py
@@ -1,13 +1,13 @@
 import sys
 
 def isPalindrome(s):
-    return recursion(s,0,len(s)-2,1)
+    return recursion(s,0,len(s)-2)
 
-def recursion(s,i,l,n):
-    if(i>=l-i): return 1,n
+def recursion(s,i,l):
+    if(i>=l-i): return 1,i+1
     else:
-        if(s[i] == s[l-i]): return recursion(s,i+1,l,n+1)
-        else: return 0,n
+        if(s[i] == s[l-i]): return recursion(s,i+1,l)
+        else: return 0,i+1
 
 T = int(sys.stdin.readline())
 
