aea9a4331941ff34211ae437118e4304c5dee831
Youngini <hardworkingini@gmail.com>
2022-09-11 23:09:51 +0900

    [집합과 맵] 숫자 카드_defaultdic 사용

---
+++ baek10815_py.py
@@ -0,0 +1,11 @@
+import sys
+from collections import defaultdict
+N = int(sys.stdin.readline())
+ary = defaultdict(int) 
+for x in sys.stdin.readline().split():ary[x] = 1
+M = int(sys.stdin.readline())
+for x in sys.stdin.readline().split():
+    print(ary[x],end = ' ')
+
+
+
